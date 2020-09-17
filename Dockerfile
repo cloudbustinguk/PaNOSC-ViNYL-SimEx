14dfff1e74475a0e34636bebd0d1169bdede5223
Daniel Webster <info@cloudbusting.io>
2020-09-17 21:01:34 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# dsw@cloudbusting.io - Daniel Webster
+FROM chuckie82/centos_env
+LABEL maintainer="Chunhong Yoon <yoon82@stanford.edu>"
+
+ADD requirements.txt /opt/requirements.txt
+ADD python_install.sh /opt/python_install.sh
+RUN yum remove -y cmake && yum install -y cmake3 hdf5-devel flex bison   && \
+    yum clean all && rm -rf /var/cache/yum                               && \
+    update-alternatives --install /usr/bin/cmake cmake /usr/bin/cmake3 3 && \
+    /opt/python_install.sh
+ENV PATH /opt/miniconda/bin:$PATH
+
+ARG simex_script=simex_install.sh
+ADD $simex_script /opt/simex_install.sh
+
+RUN ["bash", "/opt/simex_install.sh"]
+
+ENV SIMEX_ROOT=/opt/simex_platform
+ENV PATH=$SIMEX_ROOT/bin:$PATH
+ENV PYTHONPATH=$SIMEX_ROOT/Sources/python:$SIMEX_ROOT/lib/python3.6:$PYTHONPATH
+ENV SIMEX_TESTS=$SIMEX_ROOT/Tests
+ENV PYFAI_TESTIMAGES=/tmp
+
---
+++ simex_install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+set -e
+
+BRANCH=develop
+URL=https://github.com/PaNOSC-ViNYL/SimEx/archive/${BRANCH}.zip
+
+cd /opt
+
+wget $URL
+unzip ${BRANCH}.zip
+rm ${BRANCH}.zip
+cd SimEx-${BRANCH}
+
+export PATH=/opt/miniconda/bin:$PATH
+#export HDF5_ROOT=/opt/miniconda
+
+echo "###### DONE unzip ${BRANCH}.zip"
+
+echo "DEBUG: $PATH"
+conda install -c intel mkl
+
+echo "##### DONE install mkl"
+
+ROOT_DIR=/opt/simex_platform
+mkdir -p $ROOT_DIR
+
+# Create new build dir and cd into it.
+
+mkdir -v build
+cd build
+
+# Uncomment the next line and specify the install dir for a custom user install.
+#cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
+# Uncomment the next line and specify the install dir for a developer install.
+##cmake -DXCSITPhotonDetector=OFF \
+      #-DGAPDPhotonDiffractor=OFF \
+      #-DCrystFELPhotonDiffractor=OFF \
+      #-DINSTALL_TESTS=OFF \
+      #-DSRW_OPTIMIZED=ON \
+      #-DDEVELOPER_INSTALL=OFF \
+      #-DCMAKE_INSTALL_PREFIX=$ROOT_DIR \
+      #$ROOT_DIR \
+      #..
+
+# DSW - Cloudbusting
+cmake -DSRW_OPTIMIZED=ON \
+      -DUSE_SingFELPhotonDiffractor=ON \
+      -DUSE_CrystFELPhotonDiffractor=ON \
+      -DUSE_s2e=ON \
+      -DUSE_S2EReconstruction_EMC=ON \
+      -DUSE_S2EReconstruction_DM=ON \
+      -DUSE_wpg=ON \
+      -DUSE_GenesisPhotonSource=ON \
+      -DUSE_FEFFPhotonInteractor=ON \
+      -DINSTALL_TESTS=OFF \
+      -DDEVELOPER_INSTALL=OFF \
+      -DCMAKE_INSTALL_PREFIX=$ROOT_DIR \
+      $ROOT_DIR \
+      ..
+
+chmod og+rwX -R $ROOT_DIR
+
+# Build the project.
+make
+
+echo "######## done make"
+
+# Install the project.
+make install
+cd ../..
+
+echo "####### done make install"
+
+#rm -rf simex_platform-${BRANCH}
+
+
+#remove tests?
+#rm -rf $ROOT_DIR/Tests
+
+echo "source /opt/simex_platform/bin/simex_vars.sh" > /etc/profile.d/scripts-simex.sh && \
+	chmod 755 /etc/profile.d/scripts-simex.sh
+
+
+echo "export PYFAI_TESTIMAGES=/tmp" >> /etc/profile.d/scripts-simex.sh
+
+
+chmod og+rwX -R /opt/simex_platform
+
